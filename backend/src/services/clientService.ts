1d1b25df541b12057a4ad05d906f9206f8871108
Parvus8 <pigmeu.reddit2@gmail.com>
2026-06-12 19:26:11 -0300

    ajustes na parte de serviços e criação do arquivo controller

--- backend/src/services/clientService.ts
+++ backend/src/services/clientService.ts
@@ -1,7 +1,34 @@
 import { Cliente } from "../data/cliente.ts";
+import axios from "axios";
 
 class ClienteService {
+
     private clientes: Cliente[] = [];
+
+    async loadClientes() {
+        try {
+
+            const response =
+                await axios.get(URL);
+
+            this.clientes =
+                response.data.results.map(
+                    (cliente: any, index: number) => ({
+                        ...cliente,
+                        id: String(index + 1)
+                    })
+                );
+
+        } catch (error) {
+
+            console.error(
+                "Erro ao carregar clientes",
+                error
+            );
+
+            throw error;
+        }
+    }
 
     setClientes(clientes: Cliente[]) {
         this.clientes = clientes;
@@ -13,14 +40,14 @@ class ClienteService {
         state?: string,
         name?: string
     ) {
+        page = Math.max(1, page);
+        limit = Math.max(1, limit);
         let result = [...this.clientes];
 
         if (state) {
             result = result.filter(
                 client =>
-                    client.location.state
-                        .toLowerCase()
-                        .includes(state.toLowerCase())
+                    client.location.state.toLowerCase() === state.toLowerCase()
             );
         }
 
@@ -28,8 +55,7 @@ class ClienteService {
             result = result.filter(client => {
 
                 const fullName =
-                    `${client.name.first} ${client.name.last}`;
-
+                    `${client.name.title} ${client.name.first} ${client.name.last}`;
                 return fullName
                     .toLowerCase()
                     .includes(name.toLowerCase());
@@ -42,15 +68,17 @@ class ClienteService {
             (page - 1) * limit;
 
         return {
-            total,
-            page,
-            limit,
-            data: result.slice(
-                start,
-                start + limit
-            )
+            total, page, limit,
+            data: result.slice(start, start + limit)
         };
     }
+
+    getClientById(id: string) { // busca por id
+        return this.clientes.find(
+            cliente => cliente.id === id
+        );
+    }
+
 }
 
 export default new ClienteService();
