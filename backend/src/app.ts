3ebf7e075f6d155b964ea8e0a6cc4829d503d05e
Parvus8 <pigmeu.reddit2@gmail.com>
2026-06-14 12:24:51 -0300

    adição de cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import clientRoutes from "./routes/clienteRoutes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", clientRoutes);
 
 export default app;
