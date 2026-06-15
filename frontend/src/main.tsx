056a2df07204048c7ff711d4605d4e84cdfe6256
Parvus8 <pigmeu.reddit2@gmail.com>
2026-06-15 01:41:08 -0300

    adicionado o strict mode faltante no main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./app";
+import { StrictMode } from "react";
 
 ReactDOM
     .createRoot(
         document.getElementById("root")!
     )
     .render(
-        <App />
+        <StrictMode>
+            <App />
+        </StrictMode>
     );
